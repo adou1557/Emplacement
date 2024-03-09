cae0566a8a8a7a14be36795fb1c8ff9c917dbcc0
ahmad <ahmad@DESKTOP-FSH9L0B>
2024-03-09 17:27:31 +0100

    Add project files.

---
+++ FWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
